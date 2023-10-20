9a50989469fbd9e81ea5c6280c17f6e155cb2c1c
hhuzzz <hhuzzz124@163.com>
2023-10-20 23:41:59 +0800

    test commit

--- example/testserver.cc
+++ example/testserver.cc
@@ -1,54 +1,45 @@
-#include "TcpServer.h"
 #include "Logger.h"
+#include "TcpServer.h"
 
-#include <string>
 #include <functional>
+#include <string>
 
-class EchoServer
-{
+class EchoServer {
 public:
-    EchoServer(EventLoop *loop,
-            const InetAddress &addr, 
-            const std::string &name)
-        : server_(loop, addr, name)
-        , loop_(loop)
-    {
+    EchoServer(EventLoop *loop, const InetAddress &addr,
+               const std::string &name)
+        : server_(loop, addr, name), loop_(loop) {
         // 注册回调函数
         server_.setConnectionCallback(
-            std::bind(&EchoServer::onConnection, this, std::placeholders::_1)
-        );
+            std::bind(&EchoServer::onConnection, this, std::placeholders::_1));
 
         server_.setMessageCallback(
-            std::bind(&EchoServer::onMessage, this,
-                std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)
+            std::bind(&EchoServer::onMessage, this, std::placeholders::_1,
+                      std::placeholders::_2, std::placeholders::_3)
+
         );
 
         // 设置合适的loop线程数量 loopthread
         server_.setThreadNum(3);
     }
-    void start()
-    {
+    void start() {
         server_.start();
     }
+
 private:
     // 连接建立或者断开的回调
-    void onConnection(const TcpConnectionPtr &conn)
-    {
-        if (conn->connected())
-        {
-            LOG_INFO("Connection UP : %s", conn->peerAddress().toIpPort().c_str());
-        }
-        else
-        {
-            LOG_INFO("Connection DOWN : %s", conn->peerAddress().toIpPort().c_str());
+    void onConnection(const TcpConnectionPtr &conn) {
+        if (conn->connected()) {
+            LOG_INFO("Connection UP : %s",
+                     conn->peerAddress().toIpPort().c_str());
+        } else {
+            LOG_INFO("Connection DOWN : %s",
+                     conn->peerAddress().toIpPort().c_str());
         }
     }
 
     // 可读写事件回调
-    void onMessage(const TcpConnectionPtr &conn,
-                Buffer *buf,
-                Timestamp time)
-    {
+    void onMessage(const TcpConnectionPtr &conn, Buffer *buf, Timestamp time) {
         std::string msg = buf->retrieveAllAsString();
         conn->send(msg);
         conn->shutdown(); // 写端   EPOLLHUP =》 closeCallback_
@@ -58,13 +49,15 @@ private:
     TcpServer server_;
 };
 
-int main()
-{
+int main() {
     EventLoop loop;
     InetAddress addr(8000);
-    EchoServer server(&loop, addr, "EchoServer-01"); // Acceptor non-blocking listenfd  create bind 
-    server.start(); // listen  loopthread  listenfd => acceptChannel => mainLoop =>
-    loop.loop(); // 启动mainLoop的底层Poller
+    EchoServer server(
+        &loop, addr,
+        "EchoServer-01"); // Acceptor non-blocking listenfd  create bind
+    server.start(); // listen  loopthread  listenfd => acceptChannel => mainLoop
+                    // =>
+    loop.loop();    // 启动mainLoop的底层Poller
 
     return 0;
 }
